8498dcac8b1a38a7135695bef58e63664047a6d4
Nicholas Tate <ntate22@gmail.com>
2016-06-27 19:17:11 -0400

    converted application.js to es6

--- application.js
+++ application.js
@@ -1,8 +1,11 @@
-var LegionD = require([__dirname, 'legiond'].join('/'));
-var pkg = require([__dirname, 'package'].join('/'));
+'use strict';
 
-module.exports = function(options){
-    var legiond = new LegionD(options);
+const LegionD = require('./legiond');
+const pkg = require('./package.json');
+
+module.exports = function(options) {
+    const legiond = new LegionD(options);
     legiond.version = pkg.version;
+
     return legiond;
 }
