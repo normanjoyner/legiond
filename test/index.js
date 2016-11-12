be7da105e3f337fe8254a03a00771d21ff26ecc0
Nicholas Tate <ntate22@gmail.com>
2016-11-12 17:31:31 -0500

    starting to replace bad crypto module with built int node crypto

--- test/index.js
+++ test/index.js
@@ -1,1 +1,28 @@
-console.log('No tests yet!');
+const assert = require('assert');
+const crypto = require('../lib/crypto');
+
+describe('lib/crypto', function() {
+    describe('#encrypt/#decrypt()', function() {
+        it('should successfully encrypt and decrypt a value', function(done) {
+            const plaintext = 'Hello, I am going to be encrypted';
+            const node_id = '12345';
+
+            crypto.generate_dh();
+            crypto.add_id(node_id, crypto.get_dh_secret(crypto.pubkey));
+
+            return crypto.encrypt({
+                id: node_id,
+                target: node_id,
+                data: plaintext
+            }, function(err, encrypted) {
+                if (err) {
+                    return done(err);
+                }
+
+                const decrypted = crypto.decrypt(encrypted);
+                assert.equal(plaintext, decrypted);
+                return done();
+            });
+        });
+    });
+});
