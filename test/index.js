b3ec6280fe1141185f9bbc4868aab7a74553cff4
Nicholas Tate <ntate22@gmail.com>
2016-11-12 19:53:37 -0500

    fixed routing id

--- test/index.js
+++ test/index.js
@@ -2,27 +2,37 @@ const assert = require('assert');
 const crypto = require('../lib/crypto');
 
 describe('lib/crypto', function() {
-    describe('#encrypt/#decrypt()', function() {
-        it('should successfully encrypt and decrypt a value', function(done) {
-            const plaintext = 'Hello, I am going to be encrypted';
-            const node_id = '12345';
+  describe('#encrypt/#decrypt()', function() {
+    let node_id;
 
-            crypto.generate_dh();
-            crypto.add_id(node_id, crypto.get_dh_secret(crypto.pubkey));
-
-            return crypto.encrypt({
-                id: node_id,
-                target: node_id,
-                data: plaintext
-            }, function(err, encrypted) {
-                if (err) {
-                    return done(err);
-                }
-
-                const decrypted = crypto.decrypt(encrypted);
-                assert.equal(plaintext, decrypted);
-                return done();
-            });
-        });
+    before((done) => {
+      crypto.generate_node_id((err, id) => {
+        node_id = id;
+        return done(err);
+      });
     });
+
+    it('should successfully encrypt and decrypt a value', function(done) {
+      const plaintext = 'Hello, I am going to be encrypted';
+
+      crypto.generate_dh();
+      const dh_secret = crypto.generate_dh_secret(crypto.pubkey, crypto.prime);
+      console.log(dh_secret.secret);
+      crypto.add_id(node_id, dh_secret.secret);
+
+      return crypto.encrypt({
+        id: node_id,
+        target: node_id,
+        data: plaintext
+      }, function(err, encrypted) {
+        if (err) {
+          return done(err);
+        }
+
+        const decrypted = crypto.decrypt(encrypted);
+        assert.equal(plaintext, decrypted);
+        return done();
+      });
+    });
+  });
 });
