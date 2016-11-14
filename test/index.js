6939b35d20b66dcdc01c27bc663afaae629cd206
Nicholas Tate <ntate22@gmail.com>
2016-11-14 09:18:10 -0500

    handle case where auth is tampered with

--- test/index.js
+++ test/index.js
@@ -34,5 +34,33 @@ describe('lib/crypto', function() {
         return done();
       });
     });
+
+    it('should return null if auth tag is tampered with', function(done) {
+      const plaintext = 'Hello, I am going to be tampered with encrypted';
+
+      crypto.generate_dh();
+      const dh_secret = crypto.generate_dh_secret(crypto.pubkey, crypto.prime);
+      console.log(dh_secret.secret);
+      crypto.add_id(node_id, dh_secret.secret);
+
+      return crypto.encrypt({
+        id: node_id,
+        target: node_id,
+        data: plaintext
+      }, function(err, encrypted) {
+        if (err) {
+          return done(err);
+        }
+
+        crypto.generate_dh();
+        const dh_secret = crypto.generate_dh_secret(crypto.pubkey, crypto.prime);
+        const tampered_secret = dh_secret.secret;
+        crypto.add_id(node_id, tampered_secret);
+
+        const decrypted = crypto.decrypt(encrypted);
+        assert.equal(null, decrypted);
+        return done();
+      });
+    });
   });
 });
